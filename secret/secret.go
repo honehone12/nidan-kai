898cead4adde4f971864b604f81dd08ec1c8bf1e
Twelfthonehone <95571424+honehone12@users.noreply.github.com>
2025-12-23 19:06:32 +0900

    added secret gen

--- secret/secret.go
+++ secret/secret.go
@@ -1,3 +1,15 @@
 package secret
 
+import "crypto/rand"
+
 const SECRET_LEN = 32
+
+func GenerateSecret() ([]byte, error) {
+	sec := make([]byte, SECRET_LEN)
+	_, err := rand.Read(sec)
+	if err != nil {
+		return nil, err
+	}
+
+	return sec, nil
+}
